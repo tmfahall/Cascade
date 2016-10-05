ab6f2dde31bc052b26e856d8af1bf38e2ec7198e
tmfahall <tmfahall@gmail.com>
2016-10-04 18:52:49 -0700

    fixed logo

--- Scripts/Cascade-Script-Logo-Resizer.js
+++ Scripts/Cascade-Script-Logo-Resizer.js
@@ -7,16 +7,13 @@
 
         //MANUAL OVERRIDE ENTER WHOLE NUMBER OF % CHANGE TO LOGO SIZE (ie 50% = 50)
         //var change = 95;
-        console.log(winWid)
         var change = (winWid*1) / 18
-        console.log(change)
 
         var newWid = change / 100 * logoWid;
         $('#site-logo').width(newWid);
 
         var newHt = change / 100 * logoHt;
         $('#site-logo').height(newHt);
-
     })
 
 
--- Scripts/Cascade-Script-Repaint.js
+++ Scripts/Cascade-Script-Repaint.js
@@ -1,15 +1,10 @@
 ﻿var commentHeight = -1;
-var counter = 1;
 
 $('.comment-box').each(function () {
-    console.log("first run - box " + counter + " comment length = " + $(this).height() + " current box size " + commentHeight);
-    counter++;
     commentHeight = commentHeight > $(this).height() ? commentHeight : $(this).height() + 180;
 });
 
 $('.comment-box').each(function () {
-    console.log("second run - box " + counter + " comment length = " + $(this).height() + " current box size " + commentHeight);
-    counter++;
     $(this).height(commentHeight);
 });
 
